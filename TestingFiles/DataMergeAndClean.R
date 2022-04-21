05bf4ec199c9f1553c71423291e0675b27205b17
olbeck <olb5053@psu.edu>
2022-04-21 09:45:24 -0400

    All Data

--- TestingFiles/DataMergeAndClean.R
+++ TestingFiles/DataMergeAndClean.R
@@ -1,7 +1,7 @@
 
 #Load Data
 load("~/Dropbox/Olivia/Conflict/school/SODA501/FinalProject_SODA501/SODA501Project/Mitch_Data.Rdata")
-mitch_dat <- summarized_data
+mitch_dat <- mitch_Data
 
 load("~/Dropbox/Olivia/Conflict/school/SODA501/FinalProject_SODA501/SODA501Project/Olivia_Data.Rdata")
 olivia_dat <- summarized_data
@@ -16,5 +16,6 @@ dat_all <- rbind(joe_dat, mitch_dat, olivia_dat)
 
 rm(joe_dat, mitch_dat, olivia_dat)
 
+save(dat_all, file = "/Users/oliviabeck/Dropbox/Olivia/Conflict/school/SODA501/FinalProject_SODA501/SODA501Project/All_Data.Rdata")
 
 #Cleaning Data
